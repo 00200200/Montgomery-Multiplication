a85126172775d3cdef8567821b3541c4155f3173
Michał Furgała <83299832+00200200@users.noreply.github.com>
2024-04-18 14:16:14 +0200

    Version 2_0_0 Implementation from Nayuki website
    Summary (https://www.nayuki.io/page/montgomery-reduction-algorithm )

--- Montgomery.py
+++ Montgomery.py
@@ -1,72 +1,36 @@
-#        https://eduinf.waw.pl/inf/alg/001_search/0009.php Mozna samemu napisac wyzwanie XD ale dla testow i analizy pozyczam  
-# def extendGCD(a,b):
-#     u,w,x,z = 1,a,0,b
-#     while w:
-#         if w < z:
-#             q = u
-#             u = x
-#             x = q
-#             q = w
-#             w = z
-#             z = q
-#         q = w // z
-#         u -= q * x
-#         w -= q * z
-#     if z == 1:
-#         if x < 0: x += b
-#         return x
-#     else:
-#         print("BRAK")
+# https://www.nayuki.io/page/montgomery-reduction-algorithm
 
-# GCD REKRURENCYJNIE 
 def extendGCD(a, b):
     if a == 0:
         return (b, 0, 1)
     else:
         gcd, x, y = extendGCD(b % a, a)
         return (gcd, y - (b // a) * x, x)
+    
 
 def montgomeryMultiplication(A, B, N):
 
     R = 1 << (N.bit_length())  
-
     _, R_inv, _ = extendGCD(R, N)  
     R_inv = R_inv % N # ODwrotnosc R 
 
-    # Montgomery Transformacja
-    AR = (A * R) % N
-    BR = (B * R) % N
-    T = (AR * BR) % N  # Mnozenie w przestrzeni Montgomery'ego
+    K = (R*(R_inv % N) -1)/N             # // N  ? 
+    am  = A*R % N 
+    bm  = B*R % N
+    X = am * bm
+    s = (X*K % R)
+    t = X + s*N
+    u = t /R             # //N  ?
+    cc = u if u < N else u - N;
+    c = cc*R_inv % N
+    return c
 
-    _, N_inv, _ = extendGCD(N, R)
-    N_inv = (-N_inv) % R    # Odwrotnosc N 
 
-    # Montgomery reudkcja
-    m = (T * N_inv) % R
-    U = (T + m * N) // R
-    # Upewaniamy się  czy U [0,N-1] znalazłem na wikipedii bez tego w niektórych przypadkach był błąd 
-    if U >= N:
-        U -= N
-    #MOntgomery transdformacja 
-    result = (U * R_inv) % N
 
-    return result
 
-# Example usage
 A = int(input("Podaj A: "))
 B = int(input("Podaj B: "))
 N = int(input("Podaj N(LICZBA PIERWSZA): "))
 result = montgomeryMultiplication(A, B, N)
 print("WYNIK ALGORYTMU:", result)
 print("WYNIK POPRAWNY", A * B % N)
-
-
-# https://www.nayuki.io/page/montgomery-reduction-algorithm
-
-# https://en.wikipedia.org/wiki/Montgomery_modular_multiplication
-
-# https://cp-algorithms.com/algebra/montgomery_multiplication.html
-# https://en.algorithmica.org/hpc/number-theory/montgomery/
-
-# NOTATKI POLECAM SOBIE PRZYKLAD ZROBIC 
-# https://whiteboard.office.com/me/whiteboards/p/c3BvOmh0dHBzOi8vcG9saXRlY2huaWthd3JvY2xhd3NrYS1teS5zaGFyZXBvaW50LmNvbS9wZXJzb25hbC8yNzYyMDRfc3R1ZGVudF9wd3JfZWR1X3Bs/b!j45tsbHQMk-4ckJnX4ZgE-AtSja5THtLqfgCb8OmHq4Uh771Y3xuTZylmRIDSbim/01J73P2KTBGBJFNFS4CRCJQUUZG46PIIIC 
--- README.md
+++ README.md
@@ -1,36 +1,40 @@
 ## Montgomery-Multiplication
+
 ### OIAK - PROJEKT
 
 #### Cel Projektu:
-Zrozumienie, implementacja  i przedstawienie algorytmu Montgomery'ego. Opisanie jego użycia w praktyce (przy jakich warunkach oplaca sie go uzywac). Porównanie szybkości różnych podejść. 
+
+Zrozumienie, implementacja i przedstawienie algorytmu Montgomery'ego. Opisanie jego użycia w praktyce (przy jakich warunkach oplaca sie go uzywac). Porównanie szybkości różnych podejść.
 
 #### Narzędzia i technologie:
+
 - C
 - Python
 
 #### Plan Realizacji Projektu
-- Ogólne zapozanie się z tematem 
-- Zrozumienie matematyczne 
-- Wypisanie algorytmu w krokach 
-- Implementacja w Pythonie w celu lepszego zrozumienia 
-- Implementacja C (zwykła i przyspieszona) 
-- Badanie szybkosci bez optymalizacji kompilatora (naiwne podejcie, zywkly, przyspieszony) 
-- Stworzenie sprawozdania, analiza wynikow i wnioski 
 
+- Ogólne zapozanie się z tematem
+- Zrozumienie matematyczne
+- Wypisanie algorytmu w krokach
+- Implementacja w Pythonie w celu lepszego zrozumienia
+- Implementacja C (zwykła i przyspieszona)
+- Badanie szybkosci bez optymalizacji kompilatora (naiwne podejcie, zywkly, przyspieszony)
+- Stworzenie sprawozdania, analiza wynikow i wnioski
 
- krok po kroku na przykladzie:
+krok po kroku na przykladzie:
+
 ```
 a = 3, b = 5, n = 7
 
-r = 2^Ceiling[Log[2, n]] = 8	
+r = 2^Ceiling[Log[2, n]] = 8
 rinv = PowerMod[r, -1, n] = 1	#z rozszerzonego algorytmu euklidesa (odwrotnosc modularna)
 k = (r * rinv - 1) / n; = (8 * 1 - 1) / 7 = 1
 aa = Mod[a * r, n] = 3*8 mod 7 = 3
 bb = Mod[b * r, n] = 5*8 mod 7 = 5
 x = aa * bb = 3*5 = 15
 s = Mod[x * k, r] = 15 * 1 mod 8 = 7 #w programie zamieniamy na & (operacje logiczna and)
-t = x + s * n = 15 + 7 * 8 = 71
-u = t / r = 71 / 8 = 8
+t = x + s * n = 15 + 7 * 7 = 64
+u = t / r = 64 / 8 = 8
 
 cc = If[u < n, u, u - n] -> !(8 < 7) -> cc = 8 - 7 = 1
 
